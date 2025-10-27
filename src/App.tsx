45c466bd52974078d0dbb6903e95ae6a27efe5ad
Catherine Hsu <catherine.hsu.dev@gmail.com>
2025-10-27 13:42:13 +0800

    remove unused pages from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import SigninForm from "./_auth/forms/SigninForm";
 import {
   Home,
-  AllUsers,
   CreatePost,
   EditPost,
   PostDetails,
-  Profile,
-  UpdateProfile,
   Explore,
-  Saved,
 } from "./_root/pages";
 import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
@@ -30,14 +26,10 @@ function App() {
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
           <Route path="/explore" element={<Explore />} />
-          <Route path="/saved" element={<Saved />} />
-          <Route path="/all-users" element={<AllUsers />} />
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="/update-post/:id" element={<EditPost />} />
           <Route path="/posts/:id" element={<PostDetails />} />
           {/* :id/* "*"表示所有profile後面寫的都指向這個id的profile */}
-          <Route path="/profile/:id/*" element={<Profile />} />
-          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
